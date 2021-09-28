525c56325084e6210731765e28cb6d3eb983eed6
xemath <carlosfernandocastroposada@gmail.com>
2021-09-28 15:16:25 -0500

    asdasd

---
+++ validaciones.js
@@ -0,0 +1,19 @@
+//let string = "Aa";
+let inputNombreUsuario = document.getElementById('dato_nombre_usuario'); // le quito el value, por que si pone el value al principio porque el valor de lo que hay al inicio es ''
+
+
+function validar_nombre_usuario(string){
+    let patron = /^[A-Z]{1}[a-z](\s[A-Z]{1}[a-z])*\S$/g;
+    this.addEventListener('submit', (e)=>{ // puse la funcion en onsubmit en la etiqueta de la forma del html // el this, significa este elemento
+        e.preventDefault(); //prevengo la accion por default cuando apreta el boton aceptar para que no se recargue la pagina
+        if(patron.test(string)==false){
+            return false;
+        }
+        else{
+            return true;
+        }        
+    })
+}
+
+
+
